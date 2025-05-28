fc0a8245eaf63ca0fd45a493525d3d4c32afbaad
yurbass1981 <yurbass1@rambler.ru>
2025-05-28 20:45:33 +0300

    add new function s21_to_lower

--- src/s21_string_function.c
+++ src/s21_string_function.c
@@ -3,46 +3,71 @@
 #include <stdio.h>
 
 void *s21_memchr(const void *str, int c, s21_size_t n) {
-    void *result = S21_NULL;
-    const unsigned char *s = str;
-    for (s21_size_t i = 0; i < n && result == S21_NULL; i++) {
-        if (s[i] == (unsigned char)c) {
-            result = (void *)(s + i);
-        } 
+  void *result = S21_NULL;
+  const unsigned char *s = str;
+  for (s21_size_t i = 0; i < n && result == S21_NULL; i++) {
+    if (s[i] == (unsigned char)c) {
+      result = (void *)(s + i);
     }
-    return result;
+  }
+  return result;
 }
 
 size_t s21_strlen(const char *str) {
-    if (!str) {
-        return 0;
-    }
-    unsigned int count = 0;
-    while (str[count] != '\0') {
-        count++;
-    }
-    return count;
+  if (!str) {
+    return 0;
+  }
+  unsigned int count = 0;
+  while (str[count] != '\0') {
+    count++;
+  }
+  return count;
 }
 
 void *s21_to_upper(const char *str) {
-    if (!str) {
-        return S21_NULL;
-    }
-    // Вычисление длины строки
-    size_t len = s21_strlen(str);
+  if (!str) {
+    return S21_NULL;
+  }
+  // Вычисляем длину строки
+  size_t len = 0;
+  while (str[len]) len++;
 
-    // Выделение памяти под новую строку (+1 для нуль-терминатора)
-    char *result = (char *)malloc((len + 1) * sizeof(char));
-    if (result == S21_NULL) {
-        return S21_NULL;
-    }
+  // Выделяем память под новую строку (+1 для нуль-терминатора)
+  char *result = malloc(len + 1);
+  if (result == S21_NULL) {
+    return S21_NULL;
+  }
 
-    for (size_t i = 0; i <= len; i++) {
-        if (str[i] >= 'a' && str[i] <= 'z') {
-            result[i] = str[i] - 32; // Переводим в верхний регистр
-        } else {
-            result[i] = str[i]; // Копируем без изменений
-        }
+  for (size_t i = 0; i <= len; i++) {
+    if (str[i] >= 'a' && str[i] <= 'z') {
+      result[i] = str[i] - ('a' - 'A');  // Переводим в верхний регистр
+    } else {
+      result[i] = str[i];  // Копируем без изменений
     }
-    return result;
+  }
+  return result;
+}
+
+void *s21_to_lower(const char *str) {
+  if (!str) {
+    return S21_NULL;
+  }
+  // Вычисляем длину строки
+  size_t len = 0;
+  while (str[len]) len++;
+
+  // Выделяем память под новую строку (+1 для нуль-терминатора)
+  char *result = malloc(len + 1);
+  if (result == S21_NULL) {
+    return S21_NULL;
+  }
+
+  for (size_t i = 0; i <= len; i++) {
+    if (str[i] >= 'A' && str[i] <= 'Z') {
+      result[i] = str[i] + ('a' - 'A');  // Переводим в нижний регистр
+    } else {
+      result[i] = str[i];  // Копируем без изменений
+    }
+  }
+  return result;
 }
--- src/s21_string_test.c
+++ src/s21_string_test.c
@@ -4,61 +4,61 @@
 #include "s21_string_function.h"
 
 START_TEST(s21_memchr_basic_test) {
-    const char *str = "Hello, World!";
-    char c = 'o';
-    s21_size_t n = 14;
-    void *result = s21_memchr(str, c, n);
-    ck_assert_ptr_nonnull(result);
-    ck_assert_ptr_eq(result, memchr(str, c, strlen(str)));
+  const char *str = "Hello, World!";
+  char c = 'o';
+  s21_size_t n = 14;
+  void *result = s21_memchr(str, c, n);
+  ck_assert_ptr_nonnull(result);
+  ck_assert_ptr_eq(result, memchr(str, c, strlen(str)));
 }
 END_TEST
 
 START_TEST(s21_memchr_absence_test) {
-    const char *str = "Hello, World!";
-    char c = 'B';
-    s21_size_t n = 14;
-    void *result = s21_memchr(str, c, n);
-    ck_assert_ptr_null(result);
-    ck_assert_ptr_eq(result, memchr(str, c, strlen(str)));
+  const char *str = "Hello, World!";
+  char c = 'B';
+  s21_size_t n = 14;
+  void *result = s21_memchr(str, c, n);
+  ck_assert_ptr_null(result);
+  ck_assert_ptr_eq(result, memchr(str, c, strlen(str)));
 }
 END_TEST
 
 START_TEST(s21_memchr_double_test) {
-    const char *str = "Hello, World!";
-    char c = 'l';
-    s21_size_t n = 14;
-    void *result = s21_memchr(str, c, n);
-    ck_assert_ptr_nonnull(result);
-    ck_assert_ptr_eq(result, memchr(str, c, strlen(str)));
+  const char *str = "Hello, World!";
+  char c = 'l';
+  s21_size_t n = 14;
+  void *result = s21_memchr(str, c, n);
+  ck_assert_ptr_nonnull(result);
+  ck_assert_ptr_eq(result, memchr(str, c, strlen(str)));
 }
 END_TEST
 
 Suite *s21_memchr_suite(void) {
-    Suite *s = suite_create("s21_memchr");
-    TCase *tc = tcase_create("Core");
+  Suite *s = suite_create("s21_memchr");
+  TCase *tc = tcase_create("Core");
 
-    tcase_add_test(tc, s21_memchr_basic_test);
-    tcase_add_test(tc, s21_memchr_absence_test);
-    tcase_add_test(tc, s21_memchr_double_test);
-    suite_add_tcase(s, tc);
-    
-    return s;
+  tcase_add_test(tc, s21_memchr_basic_test);
+  tcase_add_test(tc, s21_memchr_absence_test);
+  tcase_add_test(tc, s21_memchr_double_test);
+  suite_add_tcase(s, tc);
+
+  return s;
 }
 
 int main(void) {
-    int number_failed;
-    int result;
-    Suite *s = s21_memchr_suite();
-    SRunner *sr = srunner_create(s);
+  int number_failed;
+  int result;
+  Suite *s = s21_memchr_suite();
+  SRunner *sr = srunner_create(s);
 
-    srunner_run_all(sr, CK_VERBOSE);
-    number_failed = srunner_ntests_failed(sr);
-    srunner_free(sr);
+  srunner_run_all(sr, CK_VERBOSE);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
 
-    if (number_failed == 0) {
-        result = EXIT_SUCCESS;
-    } else {
-        result = EXIT_FAILURE;
-    }
-    return result;
+  if (number_failed == 0) {
+    result = EXIT_SUCCESS;
+  } else {
+    result = EXIT_FAILURE;
+  }
+  return result;
 }
