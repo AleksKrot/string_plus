587174d6715dc2d97954052036d1515edf4263db
Judson Clymene <judsoncl@student.21-school.ru>
2025-05-30 19:41:54 +0300

    Add s21_strlen

--- src/s21_string_function.c
+++ src/s21_string_function.c
@@ -42,3 +42,10 @@ void *s21_memset(void *str, int c, s21_size_t n) {
     }
     return s;
 }
+
+s21_size_t s21_strlen(const char *str)
+{
+    s21_size_t len = 0;
+    for(; str[len]; len++);
+    return len + 1;
+} 
