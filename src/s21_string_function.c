030d4787431bb47c77e3d79c967070d54c977951
Judson Clymene <judsoncl@student.21-school.ru>
2025-05-31 13:33:53 +0300

    Add strpbrk function

--- src/s21_string_function.c
+++ src/s21_string_function.c
@@ -123,3 +123,15 @@ s21_size_t s21_strcspn(const char *str1, const char *str2) {
     }
     return result;
 }
+
+char *s21_strpbrk(const char *str1, const char *str2) {
+    void *result = S21_NULL;
+    for (s21_size_t i = 0; i < s21_strlen(str1) && result == S21_NULL; i++) {
+        for (s21_size_t j = 0; j < s21_strlen(str2); j++) {
+            if (*(str1 + i) == *(str2 + j)) {
+                result = (void *)(str1 + i);
+            }
+        }
+    }
+    return result;
+}
