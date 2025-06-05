36f63855fb91b655441e70d00c2d5e00a9688623
yurbass1981 <yurbass1@rambler.ru>
2025-06-05 21:02:00 +0300

    modified files

--- src/s21_sprintf/s21_sprintf.c
+++ src/s21_sprintf/s21_sprintf.c
@@ -5,20 +5,18 @@ int sprintf(char *str, const char *format, ...);
 
 int main() {
   char buffer[256];
-  int count = sprintf(buffer, "Hello, %c%d%%!", 'A', -100);
+  int count = sprintf(buffer, "Hi, %c=%d%%!", 'A', -100);
   printf("%s\nCount = %d", buffer, count);
 
   return 0;
 }
 
 int sprintf(char *str, const char *format, ...) {
-  va_list args;  // тип для хранения информации о текущей позиции в списке
-                 // аргументов
+  va_list args;  // Создаём объект для работы с аргументами, va_list -
+                 // специальный тип для хранения информации об аргументах.
   va_start(args,
-           format);  // ф-я инициализирует переменную args типа va_list для
-                     // доступа к дополнительным аргументам функции format — имя
-                     // последнего фиксированного параметра функции, после
-                     // которого начинаются переменные аргументы.
+           format);  // Mакрос инициализирует переменную args, т.е. привязывает
+                     // args к первому аргументу после format
 
   int count = 0;  // Счётчик записанных символов (возвращаемое значение)
 
@@ -33,12 +31,19 @@ int sprintf(char *str, const char *format, ...) {
     // Обработка спецификаторов
     format++;  // Пропускаем '%'
 
-    switch (*format++) {
+    if (*format == '%') {
+      *str++ = *format++;
+      count++;
+      continue;
+    }
+
+    switch (*format) {
       case 'c': {
         char c = (char)va_arg(args, int);  // char повышается до int в va_arg
                                            // (стандартные повышения аргументов)
         *str++ = c;
         count++;
+        format++;
         break;
       }
       case 'd': {
@@ -77,12 +82,13 @@ int sprintf(char *str, const char *format, ...) {
           *str++ = temp_buffer[--i];  // Записываем цифры в str
           count++;
         }
+        format++;
         break;
       }
       default:
         // Неподдерживаемый спецификатор - просто копируем
         *str++ = '%';
-        *str++ = *(format - 1);
+        *str++ = *format++;
         count += 2;
         break;
     }
