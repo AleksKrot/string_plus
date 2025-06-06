87000af12023b4a0b10f13f4102748b7c35e2f7d
yurbass1981 <yurbass1@rambler.ru>
2025-06-06 15:41:45 +0300

    add new specifiers

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -1,6 +1,8 @@
 #include <stdarg.h>  // Предназначена для работы с функциями с переменным числом аргументов
 #include <stdio.h>
 
+#include "s21_string_function.h"
+
 int sprintf(char *str, const char *format, ...);
 
 int main() {
@@ -37,13 +39,11 @@ int sprintf(char *str, const char *format, ...) {
       continue;
     }
 
-    switch (*format) {
+    switch (*format++) {
       case 'c': {
-        char c = (char)va_arg(args, int);  // char повышается до int в va_arg
-                                           // (стандартные повышения аргументов)
+        char c = (char)va_arg(args, int);  // char повышается до int
         *str++ = c;
         count++;
-        format++;
         break;
       }
       case 'd': {
@@ -82,13 +82,92 @@ int sprintf(char *str, const char *format, ...) {
           *str++ = temp_buffer[--i];  // Записываем цифры в str
           count++;
         }
-        format++;
+        break;
+      }
+      case 'f': {
+        double num = va_arg(args, double);
+        int precision = 6;  // Стандартная точность для %f
+
+        if (num < 0) {
+          *str++ = '-';
+          count++;
+          num = -num;
+        }
+
+        int int_part = (int)num;  // Извлечение целой части
+        double frac_part = num - int_part;  // Дробная часть
+
+        // Преобразование целой части
+        char int_buffer[20];
+        int i = 0;
+
+        if (int_part == 0) {
+          int_buffer[i++] = '0';
+        } else {
+          while (int_part > 0) {
+            int_buffer[i++] = '0' + (int_part % 10);
+            int_part /= 10;
+          }
+        }
+
+        // Запись целой части в обратном порядке
+        while (i > 0) {
+          *str++ = int_buffer[--i];
+          count++;
+        }
+
+        // Добавление точки
+        *str++ = '.';
+        count++;
+
+        // Преобразование дробной части
+        for (int j = 0; j < precision; j++) {
+          frac_part *= 10;
+          int didgit = (int)frac_part;
+          *str++ = '0' + didgit;
+          count++;
+          frac_part -= didgit;
+        }
+
+        break;
+      }
+      case 's': {
+        const char *s = va_arg(args, const char *);
+        if (s == NULL) {
+          s = "(null)";  // Обработка NULL по аналогии со стандартным sprintf
+        }
+        int length = s21_strlen(s);
+        s21_memcpy(str, s, length);
+        str += length;
+        count += length;
+        break;
+      }
+      case 'u': {
+        unsigned int num = va_arg(args, unsigned int);
+        char temp_buffer[12];  // С запасом для знака и нуль-терминатора
+        int i = 0;
+
+        // Обрабатываем 0 отдельно
+        if (num == 0) {
+          temp_buffer[i++] = '0';
+        } else {
+          // Разбираем число на цифры (в обратном порядке)
+          while (num > 0) {
+            temp_buffer[i++] = '0' + (num % 10);
+            num /= 10;
+          }
+        }
+        // Разворачиваем цифры в правильном порядке
+        while (i > 0) {
+          *str++ = temp_buffer[--i];  // Записываем цифры в str
+          count++;
+        }
         break;
       }
       default:
         // Неподдерживаемый спецификатор - просто копируем
         *str++ = '%';
-        *str++ = *format++;
+        *str++ = *(format - 1);
         count += 2;
         break;
     }
