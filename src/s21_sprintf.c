ba3f90ae5e5c62c477ff484405de111bc9812b5d
yurbass1981 <yurbass1@rambler.ru>
2025-06-07 19:24:31 +0300

    modified files

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -7,27 +7,174 @@
 
 int main() {
   Format format;
-  init_struct(&format);
 
   char buffer[256];
-  int count = sprintf(buffer, "Hi, %5c=%d", 'A', -100);
+  int count = sprintf(buffer, "Hi, %c%s%d", 'A', "World", -100);
   printf("%s\nCount = %d", buffer, count);
 
   return 0;
 }
 
-void init_struct(Format *format) {
-  format->flag.minus = false;
-  format->flag.plus = false;
-  format->flag.space = false;
-  format->flag.hashtag = false;
-  format->flag.zero = false;
+int s21_sprintf(char *str, const char *format, ...) {
+  va_list args;  // Создаём объект для работы с аргументами
+  va_start(args,
+           format);  // Mакрос инициализирует переменную args
+
+  char *start = str;
+
+  while (*format != '\0') {
+    if (*format != '%') {
+      // Обычный символ - копируем в выходную строку
+      *str++ = *format++;
+      continue;
+    }
+
+    format++;  // Пропускаем '%'
+
+    // Обработка %%
+    if (*format == '%') {
+      *str++ = *format++;
+      continue;
+    }
+
+    // Обработка пробела
+    if (*format == ' ') {
+      *str++ = *format++;
+    }
+
+    // Парсинг спецификатора
+    Format fmt;
+    format = parse_format(format, &fmt);
+    process_format(&fmt, args, &str);
+  }
+
+  *str = '\0';  // Завершаем строку
+  va_end(args);  // Освобождает ресурсы, связанные с va_list
+  return str - start;  // Количество записанных символов (без учёта '\0')
+}
+
+const char *parse_format(const char *fmt, Format *format) {
+  init_format(format);
+  const char *p = fmt;
+
+  // Парсинг флагов
+  while (*p == '-' || *p == '+' || *p == ' ' || *p == '0' || *p == '#') {
+    if (*p == '-') {
+      format->flags.minus = true;
+      format->flags.zero = false;  // '-' отменяет '0'
+    } else if (*p == '+') {
+      format->flags.plus = true;
+      format->flags.space = false;  // '+' отменяет пробел
+    } else if (*p == ' ')
+      format->flags.space = true;
+    else if (*p == '0')
+      format->flags.zero = true;
+    else if (*p == '#')
+      format->flags.hashtag = true;
+    p++;
+  }
+
+  // Парсинг ширины
+  if (*p == '*') {
+    format->width.asterisk = true;
+    p++;
+  } else if (isdigit(*p)) {
+    format->width.number = 0;
+    while (isdigit(*p)) {
+      format->width.number = format->width.number * 10 + (*p - '0');
+      p++;
+    }
+  }
+
+  // Парсинг точности
+  if (*p == '*') {
+    format->precision.asterisk = true;
+    p++;
+  } else if (*p == '.') {
+    p++;
+    format->precision.number = 0;
+    while (isdigit(*p)) {
+      format->precision.number = format->precision.number * 10 + (*p - '0');
+      p++;
+    }
+  }
+
+  // Парсинг модификаторов длины
+  if (*p == 'h') {
+    format->length.h = true;
+    p++;
+  } else if (*p == 'l') {
+    format->length.l = true;
+    p++;
+  } else if (*p == 'L') {
+    format->length.L = true;
+    p++;
+  }
+
+  // Парсинг спецификатора
+  switch (*p) {
+    case 'c':
+      format->spec.c = true;
+      break;
+    case 'd':
+      format->spec.d = true;
+      break;
+    case 'f':
+      format->spec.f = true;
+      break;
+    case 's':
+      format->spec.s = true;
+      break;
+    case 'u':
+      format->spec.u = true;
+      break;
+    case 'g':
+      format->spec.g = true;
+      break;
+    case 'G':
+      format->spec.G = true;
+      break;
+    case 'e':
+      format->spec.e = true;
+      break;
+    case 'E':
+      format->spec.E = true;
+      break;
+    case 'x':
+      format->spec.x = true;
+      break;
+    case 'X':
+      format->spec.X = true;
+      break;
+    case 'o':
+      format->spec.o = true;
+      break;
+    case 'p':
+      format->spec.p = true;
+      break;
+    case '%':
+      format->spec.percent = true;
+      break;
+    default:
+      // Неподдерживаемый спецификатор
+      return fmt;
+  }
+
+  return p + 1;
+}
+
+void init_format(Format *format) {
+  format->flags.minus = false;
+  format->flags.plus = false;
+  format->flags.space = false;
+  format->flags.hashtag = false;
+  format->flags.zero = false;
 
   format->width.number = 0;
   format->width.asterisk = false;
 
-  format->accur.number = 0;
-  format->accur.asterisk = false;
+  format->precision.number = 0;
+  format->precision.asterisk = false;
 
   format->length.h = false;
   format->length.l = false;
@@ -49,228 +196,4 @@ void init_struct(Format *format) {
   format->spec.p = false;
   format->spec.n = false;
   format->spec.percent = false;
-}
-
-int sprintf(char *str, const char *format, ...) {
-  va_list args;  // Создаём объект для работы с аргументами, va_list -
-                 // специальный тип для хранения информации об аргументах.
-  va_start(args,
-           format);  // Mакрос инициализирует переменную args, т.е. привязывает
-                     // args к первому аргументу после format
-
-  int count = 0;  // Счётчик записанных символов (возвращаемое значение)
-
-  while (*format != '\0') {
-    if (*format != '%') {
-      // Обычный символ - копируем в выходную строку
-      *str++ = *format++;
-      count++;
-      continue;
-    }
-
-    format++;  // Пропускаем '%'
-
-    // Обработка %%
-    if (*format == '%') {
-      *str++ = *format++;
-      count++;
-      continue;
-    }
-
-    // Парсинг спецификатора
-    Format spec;
-    format = parse_format_spec(format, &spec);
-
-    // Обработка аргументов
-    switch (*format++) {
-      case 'c': {
-        char c = (char)va_arg(args, int);  // char повышается до int
-        *str++ = c;
-        count++;
-        break;
-      }
-      case 'd': {
-        int num = va_arg(args, int);
-        int is_negative = 0;
-
-        // Обрабатываем отрицательные числа
-        if (num < 0) {
-          is_negative = 1;
-          num = -num;  // Делаем число положительным для обработки
-        }
-
-        // Временный буфер для хранения цифр (максимум 10 цифр для 32-битного
-        // int)
-        char temp_buffer[12];  // С запасом для знака и нуль-терминатора
-        int i = 0;
-
-        // Обрабатываем 0 отдельно
-        if (num == 0) {
-          temp_buffer[i++] = '0';
-        } else {
-          // Разбираем число на цифры (в обратном порядке)
-          while (num > 0) {
-            temp_buffer[i++] = '0' + (num % 10);
-            num /= 10;
-          }
-        }
-
-        // Добавляем минус, если число было отрицательным
-        if (is_negative) {
-          temp_buffer[i++] = '-';
-        }
-
-        // Разворачиваем цифры в правильном порядке
-        while (i > 0) {
-          *str++ = temp_buffer[--i];  // Записываем цифры в str
-          count++;
-        }
-        break;
-      }
-      case 'f': {
-        double num = va_arg(args, double);
-        int precision = 6;  // Стандартная точность для %f
-
-        if (num < 0) {
-          *str++ = '-';
-          count++;
-          num = -num;
-        }
-
-        int int_part = (int)num;  // Извлечение целой части
-        double frac_part = num - int_part;  // Дробная часть
-
-        // Преобразование целой части
-        char int_buffer[20];
-        int i = 0;
-
-        if (int_part == 0) {
-          int_buffer[i++] = '0';
-        } else {
-          while (int_part > 0) {
-            int_buffer[i++] = '0' + (int_part % 10);
-            int_part /= 10;
-          }
-        }
-
-        // Запись целой части в обратном порядке
-        while (i > 0) {
-          *str++ = int_buffer[--i];
-          count++;
-        }
-
-        // Добавление точки
-        *str++ = '.';
-        count++;
-
-        // Преобразование дробной части
-        for (int j = 0; j < precision; j++) {
-          frac_part *= 10;
-          int didgit = (int)frac_part;
-          *str++ = '0' + didgit;
-          count++;
-          frac_part -= didgit;
-        }
-
-        break;
-      }
-      case 's': {
-        const char *s = va_arg(args, const char *);
-        if (s == NULL) {
-          s = "(null)";  // Обработка NULL по аналогии со стандартным sprintf
-        }
-        int length = s21_strlen(s);
-        s21_memcpy(str, s, length);
-        str += length;
-        count += length;
-        break;
-      }
-      case 'u': {
-        unsigned int num = va_arg(args, unsigned int);
-        char temp_buffer[12];  // С запасом для знака и нуль-терминатора
-        int i = 0;
-
-        // Обрабатываем 0 отдельно
-        if (num == 0) {
-          temp_buffer[i++] = '0';
-        } else {
-          // Разбираем число на цифры (в обратном порядке)
-          while (num > 0) {
-            temp_buffer[i++] = '0' + (num % 10);
-            num /= 10;
-          }
-        }
-        // Разворачиваем цифры в правильном порядке
-        while (i > 0) {
-          *str++ = temp_buffer[--i];  // Записываем цифры в str
-          count++;
-        }
-        break;
-      }
-      default:
-        // Неподдерживаемый спецификатор - просто копируем
-        *str++ = '%';
-        *str++ = *(format - 1);
-        count += 2;
-        break;
-    }
-  }
-
-  *str = '\0';  // Завершаем строку
-  va_end(args);  // Освобождает ресурсы, связанные с va_list
-  return count;  // Количество записанных символов (без учёта '\0')
-}
-
-const char *parse_format_spec(const char *fmt, Format *spec) {
-  const char *p = fmt;
-
-  // Парсинг флагов
-  while (*p == '-' || *p == '+' || *p == ' ' || *p == '0') {
-    if (*p == '-')
-      spec->flag.minus = true;
-    else if (*p == '+')
-      spec->flag.plus = true;
-    else if (*p == ' ')
-      spec->flag.space = true;
-    else if (*p == '0')
-      spec->flag.zero = true;
-    p++;
-  }
-
-  // Парсинг ширины
-  if (*p == '*') {
-    spec->width.asterisk = true;
-    p++;
-  } else if (isdigit(*p)) {
-    spec->width.number = 0;
-    while (isdigit(*p)) {
-      spec->width.number = spec->width.number * 10 + (*p - '0');
-      p++;
-    }
-  }
-
-  // Парсинг точности
-  if (*p == '*') {
-    spec->accur.asterisk = true;
-    p++;
-  } else if (*p == '.') {
-    p++;
-    spec->accur.number = 0;
-    while (isdigit(*p)) {
-      spec->accur.number = spec->accur.number * 10 + (*p - '0');
-      p++;
-    }
-  }
-
-  // Парсинг модификаторов длины
-  if (*p == 'h') {
-    spec->length.h = true;
-    p++;
-  } else if (*p == 'l') {
-    spec->length.l = true;
-    p++;
-  } else if (*p == 'L') {
-    spec->length.L = true;
-    p++;
-  }
 }
--- src/s21_sprintf.h
+++ src/s21_sprintf.h
@@ -1,67 +1,74 @@
 #ifndef S21_SPRINTF_H
 #define S21_SPRINTF_H
 
+#include <stdarg.h>
 #include <stdbool.h>
 
 // Структура для хранения флагов
 typedef struct {
-  bool minus;
-  bool plus;
-  bool space;
+  bool minus;  // Флаг выравнивания по левому краю
+  bool plus;  // Флаг обязательного отображения знака +
+  bool space;  // Флаг отображения пробела вместо знака +
   bool hashtag;
-  bool zero;
+  bool zero;  // Флаг дополняет число слева нулями (0) вместо пробелов
 } Flags;
 
 // Структура для хранения ширины
 typedef struct {
-  int number;
-  bool asterisk;
+  int number;     // Числовое значение ширины
+  bool asterisk;  // '*'
 } Width;
 
 // Структура для хранения точности
 typedef struct {
-  int number;
-  bool asterisk;
-} Accur;
+  int number;     // Числовое значение точности
+  bool asterisk;  // '*'
+} Precision;
 
-// Структура для хранения длины
+// Структура для хранения модификаторов длины
 typedef struct {
-  bool h;
-  bool l;
-  bool L;
+  bool h;  // Модификатор short
+  bool l;  // Модификатор long
+  bool L;  // Модификатор long double
 } Length;
 
 // Структура для хранения спецификаторов
 typedef struct {
-  bool c;
-  bool d;
-  bool i;
-  bool e;
-  bool E;
-  bool f;
-  bool g;
-  bool G;
-  bool o;
-  bool s;
-  bool u;
-  bool x;
-  bool X;
-  bool p;
-  bool n;
-  bool percent;
-} Spec;
+  bool c;  // Символ
+  bool d;  // Целое число со знаком
+  bool i;  // Целое число со знаком
+  bool e;  // Вывод числа в научном формате с маленькой буквой e
+  bool E;  // Вывод числа в научном формате с заглавной буквой E
+  bool f;  // Число с плавающей точкой
+  bool g;  // Вывод чисел с плавающей точкой в компактном формате
+  bool G;  // Вывод чисел с плавающей точкой в компактном формате
+  bool o;  // Целое число в восьмеричном формате
+  bool s;  // Строка
+  bool u;  // Целое число без знака
+  bool x;  // Целое число без знака в шестнадцатеричном формате с маленькими
+           // буквами a–f
+  bool X;  // Целое число без знака в шестнадцатеричном формате с заглавными
+           // буквами A–F
+  bool p;  // Вывод указателей
+  bool n;  // Запись количества уже выведенных символов в переменную
+  bool percent;  // Символ процента
+} Specifier;
 
 typedef struct {
-  Flags flag;
-  Width width;
-  Accur accur;
-  Length length;
-  Spec spec;
+  Flags flags;          // Флаги формата
+  Width width;          // Ширина поля
+  Precision precision;  // Точность
+  Length length;        // Модификаторы длины
+  Specifier spec;       // Спецификаторы
 } Format;
 
-void init_struct(Format *format);
-
-int sprintf(char *str, const char *format, ...);
-const char *parse_format_spec(const char *fmt, Format *spec);
+// Функция парсинга строки формата
+int s21_sprintf(char *str, const char *format, ...);
+// Функция парсинга строки формата
+const char *parse_format(const char *fmt, Format *format);
+// Инициализация структуры Format значениями по умолчанию
+void init_format(Format *format);
+// Функция обработки аргументов согласно формату
+const char process_format(Format *format, va_list args, char **buffert);
 
 #endif
