4c2d425c3b97330d568892e19c596891db8b2173
Judson Clymene <judsoncl@student.21-school.ru>
2025-06-13 18:00:19 +0300

    Added header

---
+++ src/tests/s21_string_test.h
@@ -0,0 +1,20 @@
+#ifndef S21_STRING_TEST_H
+#define S21_STRING_TEST_H
+
+#include "s21_string.h"
+#include "s21_sprintf.h"
+
+#include <check.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+Suite *s21_memchr_suite(void);
+Suite *s21_memcmp_suite(void);
+Suite *s21_memcpy_suite(void);
+Suite *s21_memset_suite(void);
+Suite *s21_strchr_suite(void);
+Suite *s21_strncat_suite(void);
+Suite *s21_sprintf_suite(void);
+
+#endif
